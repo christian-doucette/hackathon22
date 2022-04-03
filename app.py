a9e4a0d313768e050d0f2ee3bbc2e1cd159053f9
Jake Apfel <jsapfel@uchicago.edu>
2022-04-02 20:20:03 -0500

    home page

--- app.py
+++ app.py
@@ -22,6 +22,10 @@ story = "\nStory time! Reply with a word to continue the story:\n"
 
 app = Flask(__name__)
 
+@app.route('/')
+def hello():
+    return 'Welcome to Story Time!'
+
 @app.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     """Send a dynamic reply to an incoming text message"""
